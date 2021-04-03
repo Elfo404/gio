cf7d63815bee5421185947a956f6325ee3586b72
Elfo404 <me@giordanoricci.com>
2021-04-03 15:50:31 +0100

    chore: reordering conf

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -3,12 +3,12 @@ module.exports = {
   plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'prettier',
     'plugin:prettier/recommended',
+    'prettier',
   ],
   rules: {
-    'prettier/prettier': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
+    'prettier/prettier': 'error',
   },
 };
