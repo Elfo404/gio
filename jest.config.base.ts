17707c48def622873f409a9e496ddbbf52ddcc35
Elfo404 <me@giordanoricci.com>
2021-11-06 18:26:13 +0000

    chore: updates

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -1,8 +1,13 @@
-export default {
+import type { InitialOptionsTsJest } from 'ts-jest/dist/types';
+
+const config: InitialOptionsTsJest = {
   preset: 'ts-jest',
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>'],
   collectCoverageFrom: ['src/**/*.tsx', 'src/**/*.ts'],
   testEnvironment: 'node',
+  collectCoverage: true,
   coveragePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
   coverageDirectory: '<rootDir>/coverage/',
 };
+
+export default config;
